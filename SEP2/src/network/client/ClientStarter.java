de56dc0d6e8a2c26414b1340e0319bab85e045f3
Luemantis <ledaodavid@gmail.com>
2019-04-11 12:46:54 +0200

    down

--- SEP2/src/network/client/ClientStarter.java
+++
@@ -1,8 +0,0 @@
-package network.client;
-
-public class ClientStarter {
-    public static void main(String[] args) {
-        Thread t = new Thread(new Client("localhost"));
-        t.start();
-    }
-}
