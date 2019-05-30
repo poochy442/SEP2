89a01287a843d34cb8bc5e5d677a8659b683b263
Luemantis <ledaodavid@gmail.com>
2019-05-30 14:39:52 +0200

    View

--- SEP2/src/readersWritters/ReadWrite.java
+++
@@ -1,8 +0,0 @@
-package readersWritters;
-
-public interface ReadWrite {
-    void acquireRead(); //if no-one is writing
-    void releaseRead();
-    void acquireWrite(); // if no-one is reading or writing
-    void releaseWrite();
-}
